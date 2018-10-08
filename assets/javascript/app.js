07e2ca5db0d7e7be599bd770a0707fbcaf334a55
NiRuaG <gaurin1107@gmail.com>
2018-10-08 16:48:12 -0500

    play/pause handler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,27 @@ $(document).ready(function() {
   var queryURL =
     "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=3";
 
+    // DOM IDs
   let $searchResults = $("#searchResults");
+  let $pauseAll = $("#pauseAll");
+  let  $playAll = $("#playAll");
+  
+
+  $(document).on("click", ".gifImg", function() {
+      console.log(`clicked gif`, this);
+
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate-url"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still-url"));
+        $(this).attr("data-state", "still");
+      }
+  });
 
   $.ajax({
     url: queryURL,
@@ -23,10 +43,25 @@ $(document).ready(function() {
     //   $("<img>").attr("src", response.data[2].images.original.url)
     // );
     $searchResults.append(
-      $("<img>").attr("src", response.data[0].images.fixed_width.url),
-      $("<img>").attr("src", response.data[1].images.fixed_width.url),
-      $("<img>").attr("src", response.data[2].images.fixed_width.url)
-    );
+         $("<img>")
+            .addClass("gifImg")
+            .attr("data-still-url"  , response.data[0].images.fixed_width_still.url)
+            .attr("data-animate-url", response.data[0].images.fixed_width      .url)
+            .attr("src"             , response.data[0].images.fixed_width_still.url)
+            .attr("data-state", "still")
+        ,$("<img>")
+            .addClass("gifImg")
+            .attr("data-still-url"  , response.data[1].images.fixed_width_still.url)
+            .attr("data-animate-url", response.data[1].images.fixed_width      .url)
+            .attr("src"             , response.data[1].images.fixed_width_still.url)
+            .attr("data-state", "still")
+        ,$("<img>")
+            .addClass("gifImg")
+            .attr("data-still-url"  , response.data[2].images.fixed_width_still.url)
+            .attr("data-animate-url", response.data[2].images.fixed_width      .url)
+            .attr("src"             , response.data[2].images.fixed_width_still.url)
+            .attr("data-state", "still")
+        );
     // $searchResults.append(
     //   $("<img>").attr("src", response.data[0].images.fixed_width_small.url),
     //   $("<img>").attr("src", response.data[1].images.fixed_width_small.url),
