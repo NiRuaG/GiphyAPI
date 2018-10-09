bab2b84e94ef78dbced51185139767969911bf7a
Andrew Gaurin <gaurin1107@gmail.com>
2018-10-09 15:36:13 -0500

    favorites

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,74 +27,123 @@ $(document).ready(function() {
   ];
 
   // Static IDs
-  let $searchTermButtonsArea = $("#searchTermButtonsArea");
-  let $searchResults = $("#searchResults");
-  let $pauseAll = $("#pauseAll");
-  let $playAll = $("#playAll");
+  const DOM_ID = {
+    searchResults: "searchResults",
+    favorites: "favorites",
+  }
+  
+  const $searchResults = $(`#${DOM_ID.searchResults}`);
+  const $favorites = $(`#${DOM_ID.favorites}`);
 
-  const DOM_CLASS_SearchButton = "searchButton";
+  let $searchTermButtonsArea = $("#searchTermButtonsArea");
+  let $clearAllResults = $("#clearAllResults");
+  let $pauseAllResults = $("#pauseAllResults");
+  let $playAllResults = $("#playAllResults");
+  let $clearAllFavs = $("#clearAllFavs");
+  let $pauseAllFavs = $("#pauseAllFavs");
+  let $playAllFavs = $("#playAllFavs");
+  let $imgResult = $("#imgResult");
+
+
+  const DOM_CLASS = {
+    searchButton: "searchButton",
+    gifImg: "gifImg",
+    rating: "rating",
+    imgResult: "imgResult",
+    favImg: "favImg",
+  };
+
+  // Dynamic Selections
+  let DOM_SELECT = {
+    gifImg_any      : `.${DOM_CLASS.gifImg}`,
+    gifImg_results  : `#${DOM_ID.searchResults} .${DOM_CLASS.gifImg}`,
+    gifImg_favorites: `#${DOM_ID.favorites} .${DOM_CLASS.gifImg}`,
+    searchButtons: `.${DOM_CLASS.searchButton}`,
+    favImg: `.${DOM_CLASS.favImg}`,
+  };
 
   topics.forEach(topic => {
     $searchTermButtonsArea.append(
       $("<button>")
         .text(topic)
-        .addClass(DOM_CLASS_SearchButton)
+        .addClass(DOM_CLASS.searchButton)
     );
   });
+  $searchResults.append(
+    $("#imgResultTemp")
+      .clone()
+      .contents()
+  );
 
-  // Dynamic Selections
-  let DOM_Selectors = {
-    gifImgs: ".gifImg",
-    searchButtons: `.${DOM_CLASS_SearchButton}`
-  };
-
-  let pauseGif = $jqResult => {
+  function pauseGif($jqResult) {
     $jqResult.each(function(index, element) {
       $(this).attr("src", $(this).attr("data-still-url"));
       $(this).attr("data-state", "still");
     });
-  };
+  }
 
-  let playGif = $jQResult => {
+  function playGif($jQResult) {
     $jQResult.each(function(index, element) {
       $(this).attr("src", $(this).attr("data-animate-url"));
       $(this).attr("data-state", "animate");
     });
-  };
+  }
 
-  $pauseAll.click(function() {
-    pauseGif($(DOM_Selectors.gifImgs));
+  $clearAllResults.click(function() {
+    $searchResults.empty();
   });
-  $playAll.click(function() {
-    playGif($(DOM_Selectors.gifImgs));
+  $pauseAllResults.click(function() {
+    pauseGif($(DOM_SELECT.gifImg_results));
+  });
+  $playAllResults.click(function() {
+    playGif($(DOM_SELECT.gifImg_results));
+  });
+  $pauseAllFavs.click(function() {
+    pauseGif($(`#favorites .${DOM_CLASS.gifImg}`));
+  });
+  $playAllFavs.click(function() {
+    playGif($(`#favorites .${DOM_CLASS.gifImg}`));
   });
 
-  $(document).on("click", DOM_Selectors.gifImgs, function(event) {
-    // Check current state, by data attri, and toggle
-    if ($(this).attr("data-state") === "still") {
-      playGif($(this));
+  $(document).on("click", DOM_SELECT.favImg, function(event) {
+    let $this = $(this);
+    $this.toggleClass("favorited");
+
+    if ($this.hasClass("favorited")){
+      $("#favorites").append($this.closest(".imgResult").clone());
+    }
+    else {
+      console.log($this.closest(".imgResult"));
+      $this.closest(".imgResult").remove();
+    }
+  });
+
+  $(document).on("click", DOM_SELECT.gifImg_any, function(event) {
+    // Check current state, by data attr, and toggle
+    let $gif = $(this);
+    if ($gif.attr("data-state") === "still") {
+      playGif($gif);
     } else {
-      pauseGif($(this));
+      pauseGif($gif);
     }
   });
 
   function searchForGifsOf(searchTerm) {
-
     let scheme = "https://";
-    let   host = "api.giphy.com";
-    let   path = "/v1/gifs/search";
+    let host = "api.giphy.com";
+    let path = "/v1/gifs/search";
 
     let queryParams = $.param({
-        api_key : "dc6zaTOxFJmzC",
-              q : searchTerm,
-          limit : 10,
-            fmt : "json"
+      api_key: "dc6zaTOxFJmzC",
+      q: searchTerm,
+      limit: 3,
+      fmt: "json"
     });
-    let queryURL = scheme+host+path+'?'+queryParams;
+    let queryURL = scheme + host + path + "?" + queryParams;
 
     $.ajax({
-         url: queryURL,
-      method: "GET",
+      url: queryURL,
+      method: "GET"
     }).then(function(response) {
       // $searchResults.append(
       //   $("<img>").attr("src", response.data[0].images["480w_still"].url),
@@ -106,26 +155,34 @@ $(document).ready(function() {
       //   $("<img>").attr("src", response.data[1].images.original.url),
       //   $("<img>").attr("src", response.data[2].images.original.url)
       // );
-      response.data.forEach( (datum) => {
-        $searchResults.append(
-            $("<img>")
-              .addClass("gifImg")
-              .attr("data-still-url", datum.images.fixed_width_still.url)
-              .attr("data-animate-url", datum.images.fixed_width.url)
-              .attr("src", datum.images.fixed_width_still.url)
-              .attr("data-state", "still"));
-      });      
+      response.data.forEach(datum => {
+        let $clone = $("#imgResultTemp").clone().contents();
+
+        $clone
+          .find("img")
+            .attr({
+              "data-still-url"  : datum.images.fixed_width_still.url,
+              "data-animate-url": datum.images.fixed_width.url,
+              "src": datum.images.fixed_width_still.url,
+              "data-state": "still"
+            });
+
+        $clone
+          .find(`.${DOM_CLASS.rating}`).text(datum.rating);
+
+        $searchResults
+          .append($clone);
+      });
       // $searchResults.append(
       //   $("<img>").attr("src", response.data[0].images.fixed_width_small.url),
       //   $("<img>").attr("src", response.data[1].images.fixed_width_small.url),
       //   $("<img>").attr("src", response.data[2].images.fixed_width_small.url)
       // );
-      console.log(response.data[0].images);
+      console.log(response);
     });
   }
 
-  $(document).on("click", `.${DOM_CLASS_SearchButton}`, function(event) {
-    //   console.log("Searching for ", $(this).text());
+  $(document).on("click", DOM_SELECT.searchButtons, function(event) {
     $searchResults.empty();
     searchForGifsOf($(this).text());
   });
